a7880429ecac146e193d8f5e3edf6910ccdd486c
sanpedrofabianezequiel <sanpedrofabianezequiel@gmail.com>
2023-02-03 17:47:58 -0300

    config error handler

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,14 @@
+export class DatabaseConnectionError extends Error {
+    reason = 'Error connecting to database';
+    statusCode = 500;
+    constructor() {
+        super('Error connecting to db');
+        // Only because we are extending a built in class
+        Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+    serializeErrors() {
+        return [
+            { message: this.reason }
+        ];
+    }
+}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { signinRouter } from './routes/signin';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
