37fb7453eff25807b9cf2d5a59bb96b6ce674340
sanpedrofabianezequiel <sanpedrofabianezequiel@gmail.com>
2023-02-05 13:25:09 -0300

    add k8s auth and user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,8 @@ import { signoutRouter } from './routes/signout';
 import { signinRouter } from './routes/signin';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
+import mongoose from 'mongoose';
+
 
 const app = express();
 app.use(json());
@@ -19,7 +21,18 @@ app.use(errorHandler);
 /*app.all('*',async (req,res,next) => {
   next(new NotFoundError());
 });*/
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+  });
+}
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!');
-});
+start();
+
+
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,17 +1,28 @@
 import express,{Request,Response} from 'express';
 import { body,validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
 router.post('/api/users/signup',[
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({ min: 4, max: 20 }).withMessage('Password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+],async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array());
     }
+
+    const { email, password } = req.body;
+    const user  = await User.findOne({ email });
+    if (user) {
+        throw new BadRequestError('Email in use');
+    }
+    const newUser = User.build({ email, password });
+    await newUser.save();
+    res.status(201).send(newUser);
 });
 
 export { router as signupRouter };
